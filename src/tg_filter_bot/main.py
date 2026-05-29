52dfcbde17ce6906f8c35957d1cc0d95ce7547bf
Luigi <guiluigiribeiro@gmail.com>
2026-05-29 11:39:10 -0300

    working fine

--- src/tg_filter_bot/main.py
+++ src/tg_filter_bot/main.py
@@ -1,2 +1,31 @@
+from telethon import TelegramClient, events, sync
+from dotenv import load_dotenv
+import os
+
+
 def main():
-    print("Hello World")
+    load_dotenv()  # loads variables from .env into environment
+
+    api_id = os.getenv("api_id")
+    api_hash = os.getenv("api_hash")
+    
+    client = TelegramClient('session_name', api_id, api_hash)
+    client.start()
+
+    mychannel = client.get_entity("https://t.me/+tZ_MCG46tWQyYjZh") 
+    crownchannel = client.get_entity("https://t.me/crowmantech")
+
+    keywords = ["ryzen 5 5500", "b450", "a520m", "b550", "kindle", "teclado", "rx"]
+
+
+    @client.on(events.NewMessage(chats=crownchannel))
+    async def handler(event):
+        
+        for word in keywords:
+            if word in event.message.text.lower():
+                await client.send_message(channel, event.message.text)
+
+    print("Listening for new messages...")
+    client.run_until_disconnected()
+
+
