9ae39ac79860e0afe45aae4fe41dfbfd935e7f89
Luigi <guiluigiribeiro@gmail.com>
2026-05-29 12:13:18 -0300

    To do

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # telegramAlertBot
 
 This is a personal project to create a Telegram user Bot that filters messages from promotions groups and send notification only for specific products.
+
+TO DO: 
+    Create a BotFather to send message
--- src/tg_filter_bot/main.py
+++ src/tg_filter_bot/main.py
@@ -15,15 +15,14 @@ def main():
     mychannel = client.get_entity("https://t.me/+tZ_MCG46tWQyYjZh") 
     crownchannel = client.get_entity("https://t.me/crowmantech")
 
-    keywords = ["ryzen 5 5500", "b450", "a520m", "b550", "kindle", "teclado", "rx"]
+    keywords = ["ryzen 5 5500", "b450", "a520m", "b550", "kindle", "teclado", "rx", "rtx", "cadeira"]
 
 
     @client.on(events.NewMessage(chats=crownchannel))
     async def handler(event):
-        
         for word in keywords:
             if word in event.message.text.lower():
-                await client.send_message(channel, event.message.text)
+                await client.send_message("me", event.message.text)
 
     print("Listening for new messages...")
     client.run_until_disconnected()
